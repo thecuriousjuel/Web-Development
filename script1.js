fb779e246ab765f00606006d68f836f4324669dc
Biswajit Basak <juelbasak@gmail.com>
2023-06-24 16:17:57 +0530

    Don't remember the last change

---
+++ script1.js
@@ -0,0 +1,9 @@
+alert("JS is cool!");
+
+let country = prompt("What country are you from?")
+console.log(country);
+
+let learn = confirm("Continue learning JS?");
+console.log(learn);
+
+console.log("I am learning JS")
